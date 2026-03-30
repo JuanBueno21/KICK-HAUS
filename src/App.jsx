e0c63f64f850245d8fe6d6af8f9a9991879f9856
JuanBueno21 <juancamilobuenomintic@gmail.com>
2026-03-30 14:39:23 -0500

    feat: connect Home page to router in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,27 @@
+// import { BrowserRouter, Routes, Route } from 'react-router-dom'
+// import Navbar from './components/Navbar'
+// import './App.css'
+
+// function App() {
+//   return (
+//     <BrowserRouter>
+//       <Navbar />
+//       <Routes>
+//         <Route path="/" element={<h1>Home</h1>} />
+//         <Route path="/sneakers" element={<h1>Sneakers</h1>} />
+//         <Route path="/ropa" element={<h1>Ropa</h1>} />
+//         <Route path="/marcas" element={<h1>Marcas</h1>} />
+//         <Route path="/nosotros" element={<h1>Nosotros</h1>} />
+//       </Routes>
+//     </BrowserRouter>
+//   )
+// }
+
+// export default App
+
 import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Navbar from './components/Navbar'
+import Home from './pages/Home'
 import './App.css'
 
 function App() {
@@ -7,7 +29,7 @@ function App() {
     <BrowserRouter>
       <Navbar />
       <Routes>
-        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/" element={<Home />} />
         <Route path="/sneakers" element={<h1>Sneakers</h1>} />
         <Route path="/ropa" element={<h1>Ropa</h1>} />
         <Route path="/marcas" element={<h1>Marcas</h1>} />
