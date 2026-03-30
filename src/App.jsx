17cc8fb4519a7389a6cfef04502b324e96c1e988
JuanBueno21 <juancamilobuenomintic@gmail.com>
2026-03-30 00:45:06 -0500

    feat: add Navbar styles and fix main.jsx entry point

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import Navbar from './components/Navbar'
 import './App.css'
 
 function App() {
   return (
-    <>
-      <h1>KICK-HOUSE</h1>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<h1>Home</h1>} />
+        <Route path="/sneakers" element={<h1>Sneakers</h1>} />
+        <Route path="/ropa" element={<h1>Ropa</h1>} />
+        <Route path="/marcas" element={<h1>Marcas</h1>} />
+        <Route path="/nosotros" element={<h1>Nosotros</h1>} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
-export default App;
+export default App
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,22 @@
-import React from 'react'
+import { Link } from 'react-router-dom'
+import './Navbar.css'
 
-const Navbar = () => {
+function Navbar() {
   return (
-    <div>Navbar</div>
+    <nav className="navbar">
+      <div className="navbar-left">
+        <Link to="/" className="navbar-logo">KICK-HAUS</Link>
+      </div>
+      <ul className="navbar-links">
+        <li><Link to="/sneakers">Sneakers</Link></li>
+        <li><Link to="/ropa">Ropa</Link></li>
+        <li><Link to="/marcas">Marcas</Link></li>
+        <li><Link to="/nosotros">Nosotros</Link></li>
+      </ul>
+      <div className="navbar-right">
+        <button className="navbar-cart">Carrito (0)</button>
+      </div>
+    </nav>
   )
 }
 
