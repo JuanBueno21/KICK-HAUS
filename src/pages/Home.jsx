6ce21cc37d9a17bb9406e3e149832a962047d020
JuanBueno21 <juancamilobuenomintic@gmail.com>
2026-03-30 23:56:37 -0500

    style: update hero to minimal white style, remove background image

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,22 @@
-import "./Home.css"
+import "./Home.css";
 
 function Home() {
   return (
     <main>
       <section className="hero">
         <div className="hero-content">
-          <p className="hero-tag">Inventario de productos</p>
-          <h1 className="hero-title">Lo mejor en<br />sneakers y ropa</h1>
-          {/* <a href="/sneakers" className="hero-btn">Ver Colección</a> */}
+          <p className="hero-tag">Nueva Colección 2025</p>
+          <h1 className="hero-title">
+            Lo mejor en <br />
+            sneakers y ropa
+          </h1>
+          <a href="/sneakers" className="hero-btn">
+            Ver colección
+          </a>
         </div>
       </section>
     </main>
-  )
+  );
 }
 
-export default Home
+export default Home;
